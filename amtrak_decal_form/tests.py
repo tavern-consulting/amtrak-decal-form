f5f8d3707b06beca4b53dfbf244e28ff04016b7a
Jason Ward <jason.ward@policystat.com>
2014-07-10 18:39:19 -0400

    refs #1: Added a few proof of concept tests.

---
+++ amtrak_decal_form/tests.py
@@ -0,0 +1,13 @@
+from django.core.urlresolvers import reverse
+from django.test import TestCase
+
+
+class SmokeTestCase(TestCase):
+    def test_GET_index_returns_200(self):
+        url = reverse('index')
+        r = self.client.get(url)
+        self.assertEqual(r.status_code, 200)
+
+    def test_100_percent_code_coverage(self):
+        from amtrak_decal_form import wsgi  # noqa
+        assert wsgi
