055ff99d648175200dd387a62e1da40171af9c10
Jason Ward <jason.ward@policystat.com>
2014-08-07 18:28:46 -0400

    refs #3: Added form tests.

--- amtrak_decal_form/tests.py
+++ amtrak_decal_form/tests.py
@@ -1,6 +1,8 @@
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 
+from amtrak_decal_form.forms import UserInfoForm, FORM_ERRORS
+
 
 class SmokeTestCase(TestCase):
     def test_GET_index_returns_200(self):
@@ -11,3 +13,55 @@ class SmokeTestCase(TestCase):
     def test_100_percent_code_coverage(self):
         from amtrak_decal_form import wsgi
         assert wsgi
+
+
+class FormTestCase(TestCase):
+    url = reverse('index')
+
+    @property
+    def params(self):
+        return {
+            'name': 'name',
+            'department': 1,
+            'phone_number': '123-456-7890',
+            'alternate_phone_number': '123-456-0987',
+            'cost_center': 'cost_center',
+            'wbs_element': 'wbs_element',
+            'account': 'account',
+        }
+
+    def test_form_error_for_missing_fields(self):
+        r = self.client.post(self.url, {})
+        for key, field in UserInfoForm.base_fields.items():
+            if field.required:
+                self.assertFormError(r, 'form', key, 'This field is required.')
+            else:
+                with self.assertRaises(AssertionError):
+                    self.assertFormError(
+                        r,
+                        'form',
+                        key,
+                        'This field is required.',
+                    )
+
+    def test_no_form_errors(self):
+        r = self.client.post(self.url, self.params)
+        for key, field in UserInfoForm.base_fields.items():
+            with self.assertRaises(AssertionError):
+                self.assertFormError(
+                    r,
+                    'form',
+                    key,
+                    'This field is required.',
+                )
+
+    def test_alternate_number_must_be_different(self):
+        params = self.params
+        params['alternate_phone_number'] = params['phone_number']
+        r = self.client.post(self.url, params)
+        self.assertFormError(
+            r,
+            'form',
+            'alternate_phone_number',
+            FORM_ERRORS['alternate_phone_number']['duplicate'],
+        )
