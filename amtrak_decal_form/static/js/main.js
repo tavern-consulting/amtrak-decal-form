c8ed28d14fc2570dcb70bbfe26451eefcbcf8823
Jason Ward <jason.ward@policystat.com>
2014-08-08 19:25:14 -0400

    refs #4: Sort of works, still needs some love. And validation.

--- amtrak_decal_form/static/js/main.js
+++ amtrak_decal_form/static/js/main.js
@@ -1,5 +1,7 @@
 $( document ).ready(function() {
     $('a.continue').click(function(e) {
         e.preventDefault();
+        $('.pane-2').removeClass('hide');
+        $('.pane-1').addClass('hide');
     });
 });
