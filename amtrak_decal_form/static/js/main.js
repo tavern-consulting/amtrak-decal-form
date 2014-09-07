725f8a6c54cc6f1cacd31688c4dd5a6fe5cdad75
Jason Ward <jason.ward@policystat.com>
2014-09-07 15:58:50 -0400

    refs #7: Use JS to show validation errors.

--- amtrak_decal_form/static/js/main.js
+++ amtrak_decal_form/static/js/main.js
@@ -1,4 +1,10 @@
 $( document ).ready(function() {
+    $('input').focus(function() {
+        $(this).removeClass('validation-error');
+    });
+    $('input').change(function() {
+        $(this).removeClass('validation-error');
+    });
     $('a.continue').click(function(e) {
         e.preventDefault();
         $.ajax({
@@ -10,6 +16,15 @@ $( document ).ready(function() {
                     $('#decal-spec-form').removeClass('hide');
                     $('#user-info-form').addClass('hide');
                 } else {
+                    for (var key in data.errors) {
+                        if (data.errors.hasOwnProperty(key)) {
+                            var element = $('[name*="' + key + '"]');
+                            var errorMessage = data.errors[key][0];
+                            element.addClass('validation-error');
+                            element.attr('title', errorMessage);
+                            element.tooltip();
+                        }
+                    }
                     $('.form-errors').html(data.errors);
                 }
             }
