40b38b83d4406ab7fad80fe4508c60b5f20c455a
Jason Ward <jason.ward@policystat.com>
2014-09-07 15:10:46 -0400

    refs #7: Updated the reqs, flake8 and correct panes.

--- manage.py
+++ manage.py
@@ -3,7 +3,10 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "amtrak_decal_form.settings")
+    os.environ.setdefault(
+        "DJANGO_SETTINGS_MODULE",
+        "amtrak_decal_form.settings",
+    )
 
     from django.core.management import execute_from_command_line
 
