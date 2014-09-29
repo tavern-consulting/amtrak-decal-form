21ed2cffb2f2235fa89c0cfcd960a6e162626cf3
Jason Ward <jason.ward@policystat.com>
2014-09-29 18:41:04 -0400

    refs #16: Temp.

--- amtrak_decal_form/views.py
+++ amtrak_decal_form/views.py
@@ -46,8 +46,17 @@ def index(request):
             pdf = generate_pdf(pdf_data)
             today = date.today()
             filename = 'Decal-Request-%s' % today.strftime('%m-%d-%y')
+            # TODO remove
+            mock_up_html = render_to_string(
+                'mock_up.html',
+                {
+                    'html': mark_safe(decal_form.cleaned_data['html']),
+                },
+            )
+            mock_up_data = generate_pdf(mock_up_html)
+            # TODO remove
             if request.POST.get('action') == 'preview':
-                return PDFResponse(pdf, filename)
+                return PDFResponse(mock_up_html, filename)
             else:
                 subject = 'Decal Acquisition %s' % (
                     user_form.cleaned_data['name'],
